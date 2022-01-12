06fdd2e48ca426347a2470839a03efc7c95a99ee
npantos17 <72134672+npantos17@users.noreply.github.com>
2022-01-12 01:43:52 +0100

    3 servisa.

--- models/user.js
+++ models/user.js
@@ -9,13 +9,23 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({ Orders }) {
       // define association here
+      this.hasMany(Orders, {foreignKey:'orderID', as:'orders', onDelete:'cascade', hooks:true});
     }
   };
   User.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Nije email"
+        }
+      }
+    }
   }, {
     sequelize,
     modelName: 'User',

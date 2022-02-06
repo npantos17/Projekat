500e7f2789cb9eca410c964f208b5468b52da4c0
npantos17 <72134672+npantos17@users.noreply.github.com>
2022-02-06 19:17:24 +0100

    Cors i api_login.

--- static/login.js
+++ static/login.js
@@ -8,7 +8,7 @@ function init() {
             password: document.getElementById('password').value
         };
 
-        fetch('http://127.0.0.1:9000/login', {
+        fetch('http://127.0.0.1:9000/api_login', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
